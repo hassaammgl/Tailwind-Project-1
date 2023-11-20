3bbaa6f3107f4fdd85d43a7c89ef5a7f15f719c1
hassaammgl <h455a4mmehtab@gmail.com>
2023-11-21 03:52:20 +0500

    changes added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,24 +5,142 @@ import { FaBars } from "react-icons/fa";
 import { AiOutlineClose } from "react-icons/ai";
 
 const Header = () => {
+  // const [show, setShow] = useState(false);
+  // const [width, setwidth] = useState(0);
+  // const handleOnClick = () => {
+  //   if (show) {
+  //     setShow(false);
+  //   } else if (!show) {
+  //     setShow(true);
+  //   }
+  // };
+  // useEffect(() => {
+  //   window.addEventListener("resize", () => {
+  //     setwidth(window.innerWidth);
+  //   });
+  //   console.log(width);
+  // }, [width]);
+
+  // return (
+  //   <div className="bg-gray-600 w-full md:w-[100vw] flex flex-row p-4   font-sans items-center">
+  //     <Link to="/" className="flex justify-center items-center">
+  //       <img
+  //         src={logo}
+  //         className="h-auto w-auto md:mb-0"
+  //         alt="logo for website xyz.com"
+  //       />
+  //     </Link>
+  //     {width >= 800 ? (
+  //       <ul className="flex flex-wrap pl-12 text-white items-center text-lg justify-center md:justify-around w-full md:w-[50%]">
+  //       <li className="mb-2 md:mb-0">
+  //         <Link to="/" className="hover:underline">
+  //           Menu link 1
+  //         </Link>
+  //       </li>
+  //       <li className="mb-2 md:mb-0">
+  //         <Link to="/" className="hover:underline">
+  //           Menu link 2
+  //         </Link>
+  //       </li>
+  //       <li className="mb-2 md:mb-0">
+  //         <Link to="/" className="hover:underline">
+  //           Menu link 3
+  //         </Link>
+  //       </li>
+  //       <li className="mb-2 md:mb-0">
+  //         <Link to="/" className="hover:underline">
+  //           Menu link 4
+  //         </Link>
+  //       </li>
+  //       <li className="mb-2 md:mb-0">
+  //         <Link to="/" className="hover:underline">
+  //           Menu link 5
+  //         </Link>
+  //       </li>
+  //       <li className="mb-2 md:mb-0">
+  //         <Link to="/" className="hover:underline">
+  //           Menu link 6
+  //         </Link>
+  //       </li>
+  //       <li className="mb-2 md:mb-0">
+  //         <Link to="/" className="hover:underline">
+  //           Menu link 7
+  //         </Link>
+  //       </li>
+  //     </ul>
+  //     ) : (
+  //       <button type="button" className="w-6 h-6 absolute right-6 top-6">
+  //         {show ? (
+  //           <AiOutlineClose
+  //             className="w-6 h-6 text-white"
+  //             onClick={handleOnClick}
+  //           />
+  //         ) : (
+  //           <FaBars className="w-6 h-6 text-white" onClick={handleOnClick} />
+  //         )}
+  //       </button>
+
+  //     )}
+  //     {show ? (
+  //       <div
+  //         className={`absolute z-20 bg-gray-600 top-0 p-10 w-[100vw] h-[100%] text-white list-none right-0`}
+  //         onClick={handleOnClick}
+  //       >
+  //         <button onClick={handleOnClick}>
+  //           <AiOutlineClose
+  //             className="w-6 h-6 absolute right-10 top-10 text-white"
+  //             onClick={() => setShow(false)}
+  //           />
+  //         </button>
+  //         <li className="mb-2 flex flex-col md:mb-0">
+  //           <Link to="/" className="hover:underline text-lg m-auto">
+  //             Menu link 1
+  //           </Link>
+  //           <Link to="/" className="hover:underline text-lg m-auto">
+  //             Menu link 1
+  //           </Link>
+  //           <Link to="/" className="hover:underline text-lg m-auto">
+  //             Menu link 1
+  //           </Link>
+  //           <Link to="/" className="hover:underline text-lg m-auto">
+  //             Menu link 1
+  //             </Link>
+  //           <Link to="/" className="hover:underline text-lg m-auto">
+  //             Menu link 1
+  //           </Link>
+  //           <Link to="/" className="hover:underline text-lg m-auto">
+  //             Menu link 1
+  //           </Link>
+  //           <Link to="/" className="hover:underline text-lg m-auto">
+  //             Menu link 1
+  //           </Link>
+  //         </li>
+  //       </div>
+  //     ) : (
+  //       ""
+  //     )}
+  //   </div>
   const [show, setShow] = useState(false);
-  const [width, setwidth] = useState(0);
+  const [width, setWidth] = useState(window.innerWidth);
+
   const handleOnClick = () => {
-    if (show) {
-      setShow(false);
-    } else if (!show) {
-      setShow(true);
-    }
+    setShow(!show);
   };
+
   useEffect(() => {
-    window.addEventListener("resize", () => {
-      setwidth(window.innerWidth);
-    });
-    console.log(width);
-  }, [width]);
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
-    <div className="bg-gray-600 w-full md:w-[100vw] flex flex-row p-4   font-sans items-center">
+    <div className="bg-gray-600 w-full md:w-[100vw] flex flex-row p-4 font-sans items-center relative">
       <Link to="/" className="flex justify-center items-center">
         <img
           src={logo}
@@ -32,42 +150,42 @@ const Header = () => {
       </Link>
       {width >= 800 ? (
         <ul className="flex flex-wrap pl-12 text-white items-center text-lg justify-center md:justify-around w-full md:w-[50%]">
-        <li className="mb-2 md:mb-0">
-          <Link to="/" className="hover:underline">
-            Menu link 1
-          </Link>
-        </li>
-        <li className="mb-2 md:mb-0">
-          <Link to="/" className="hover:underline">
-            Menu link 2
-          </Link>
-        </li>
-        <li className="mb-2 md:mb-0">
-          <Link to="/" className="hover:underline">
-            Menu link 3
-          </Link>
-        </li>
-        <li className="mb-2 md:mb-0">
-          <Link to="/" className="hover:underline">
-            Menu link 4
-          </Link>
-        </li>
-        <li className="mb-2 md:mb-0">
-          <Link to="/" className="hover:underline">
-            Menu link 5
-          </Link>
-        </li>
-        <li className="mb-2 md:mb-0">
-          <Link to="/" className="hover:underline">
-            Menu link 6
-          </Link>
-        </li>
-        <li className="mb-2 md:mb-0">
-          <Link to="/" className="hover:underline">
-            Menu link 7
-          </Link>
-        </li>
-      </ul>
+          <li className="mb-2 md:mb-0">
+            <Link to="/" className="hover:underline">
+              Menu link 1
+            </Link>
+          </li>
+          <li className="mb-2 md:mb-0">
+            <Link to="/" className="hover:underline">
+              Menu link 2
+            </Link>
+          </li>
+          <li className="mb-2 md:mb-0">
+            <Link to="/" className="hover:underline">
+              Menu link 3
+            </Link>
+          </li>
+          <li className="mb-2 md:mb-0">
+            <Link to="/" className="hover:underline">
+              Menu link 4
+            </Link>
+          </li>
+          <li className="mb-2 md:mb-0">
+            <Link to="/" className="hover:underline">
+              Menu link 5
+            </Link>
+          </li>
+          <li className="mb-2 md:mb-0">
+            <Link to="/" className="hover:underline">
+              Menu link 6
+            </Link>
+          </li>
+          <li className="mb-2 md:mb-0">
+            <Link to="/" className="hover:underline">
+              Menu link 7
+            </Link>
+          </li>
+        </ul>
       ) : (
         <button type="button" className="w-6 h-6 absolute right-6 top-6">
           {show ? (
@@ -79,20 +197,19 @@ const Header = () => {
             <FaBars className="w-6 h-6 text-white" onClick={handleOnClick} />
           )}
         </button>
-        
       )}
-      {show ? (
+      {show && width < 800 && (
         <div
-          className={`absolute z-20 bg-gray-600 top-0 p-10 w-[100vw] h-[100%] text-white list-none right-0`}
+          className="absolute z-20 bg-gray-600 top-0 p-10 w-[100vw] h-[100vh] text-white list-none right-0"
           onClick={handleOnClick}
         >
           <button onClick={handleOnClick}>
             <AiOutlineClose
-              className="w-6 h-6 absolute right-10 top-10 text-white"
+              className="w-6 h-6 absolute right-10 z-30 top-10 text-white"
               onClick={() => setShow(false)}
             />
           </button>
-          <li className="mb-2 flex flex-col md:mb-0">
+          <li className="mb-2 absolute flex-col z-20 bg-gray-600 top-0 p-10 w-[100vw] h-[100vh] text-white list-none right-0 flex md:mb-0">
             <Link to="/" className="hover:underline text-lg m-auto">
               Menu link 1
             </Link>
@@ -116,8 +233,6 @@ const Header = () => {
             </Link>
           </li>
         </div>
-      ) : (
-        ""
       )}
     </div>
   );
